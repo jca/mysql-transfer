0e168458a177c73f2e3bf2106995d7957bf0e8f7
Jean-Charles Callu <jc@webelop.net>
2018-02-04 21:19:05 +0000

    Interface improvements

--- sample-transfer/configuration.dist.sh
+++ sample-transfer/configuration.dist.sh
@@ -2,14 +2,14 @@
 # Distribute file: configuration.dist.sh
 # Customize in:    configuration.user.sh
 #
-# Variables used in the mysql-transfer.sh can be initialized here
+# Variables used in the mysql-transfer can be initialized here
 
 LIMITEDDATA_TABLES=()
 
 # Example with direct limit clause
-LIMITEDDATA_TABLES+=("created > NOW() - INTERVAL 1 WEEK -- tracking")
+LIMITEDDATA_TABLES+=("--where='created > NOW() - INTERVAL 1 WEEK' -- tracking")
 
 # Example with placeholder limit clause. Placeholder query is {PLACEHOLDER_NAME}_QUERY
 MIN_ORDER_ID_QUERY='SELECT min(id) FROM sales_order WHERE created > NOW() - INTERVAL 1 WEEK'
-LIMITEDDATA_TABLES+=("id > {{ MIN_ORDER_ID }} -- sales_order")
-LIMITEDDATA_TABLES+=("sales_order_id > {{ MIN_ORDER_ID }} -- sales_order_line")
+LIMITEDDATA_TABLES+=("--where='id > {{ MIN_ORDER_ID }}' -- sales_order")
+LIMITEDDATA_TABLES+=("--where='sales_order_id > {{ MIN_ORDER_ID }}' -- sales_order_line")
